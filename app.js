9614d12e9730d59c1594236567852319d4c1b6f1
Jia-Yi Jian <jyjian@illinois.edu>
2022-11-30 18:51:55 -0800

    database encryption

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-}
+});
+
+const secret = "Mybabyjasperiscutest.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("user", userSchema);
 
