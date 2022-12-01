d8180b26bc1258db20cab76b20059596165ab051
Jia-Yi Jian <jyjian@illinois.edu>
2022-11-30 16:01:19 -0800

    create user register with email and password

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const ejs = require("ejs")
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = mongoose.model("user", userSchema);
 
 app.set('view engine', 'ejs');
 
@@ -23,6 +33,20 @@ app.get("/login", function(req, res){
 app.get("/register", function(req, res){
   res.render("register");
 })
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+  newUser.save(function(err){
+    if (!err) {
+      res.render("secrets");
+    } else {
+      res.send(err);
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
